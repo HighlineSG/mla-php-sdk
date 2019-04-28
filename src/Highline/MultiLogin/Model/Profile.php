6d5952c74a723f35dffe8b7495a08dfd6a7c8910
Sebastian Sommer <mail@sebastian-sommer.com>
2019-04-28 19:01:12 +0800

    Added missing fields and moved stuff into constants

--- src/Highline/MultiLogin/Model/Profile.php
+++ src/Highline/MultiLogin/Model/Profile.php
@@ -7,12 +7,24 @@ class Profile
 {
     const OBJECT_NAME = 'profile';
 
+    const BROWSER_MIMIC = 'mimic';
+    const BROWSER_STEALTHFOX = 'stealthfox';
+
+    const OS_LINUX = 'lin';
+    const OS_MAC = 'mac';
+    const OS_WINDOWS = 'win';
+
     use Create;
 
     /**
      * @var string
      */
     public $name;
+
+    /**
+     * @var string
+     */
+    public $notes;
 
     /**
      * @var string
